245d449892630aa7aa0b3b79aa54e81a28395856
winston-lim <winston_lim1@hotmail.com>
2021-03-05 18:36:04 +0800

    Update shown weather data

--- src/utils/weatherstack.js
+++ src/utils/weatherstack.js
@@ -4,7 +4,7 @@ const getWeatherData = (data, callback) => {
   if (typeof data !== "object") {
     return;
   }
-  const url = `http://api.weatherstack.com/current?access_key=b560a570a0918134a0db665c911672f4&query=${data.latitude},${data.longitude}&units=s`;
+  const url = `http://api.weatherstack.com/forecast?access_key=b560a570a0918134a0db665c911672f4&query=${data.latitude},${data.longitude}&units=m`;
   request({ url, json: true }, (error, {body}) => { //{body} is obj destructuring; body = response.body
     if (error) {
       callback("Unable to connect to weather services");
@@ -15,7 +15,10 @@ const getWeatherData = (data, callback) => {
       return;
     }
     const currentData = body.current;
-    const information = `It is currently ${currentData.temperature} Kelvin. It feels like ${currentData.feelslike} Kelvin out.`;
+    const information = `It is currently ${currentData.temperature} degrees.
+    The weather is ${currentData.weather_descriptions[0]}. The current humidity is ${currentData.humidity}, with a cloud cover of ${currentData.cloudcover} and a wind speed of
+    ${currentData.wind_speed}
+    `;
     callback(undefined,information);
   });
 };
